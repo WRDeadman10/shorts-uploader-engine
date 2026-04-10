a3caf40dc7ce9d733c19b964553e58eb0705c119
WRDeadman10 <vinayak.lakhani@gmail.com>
2026-04-10 09:31:05 +0530

    bridge: task 2 create app/electron/ipc/systemHandlers.js

--- app/electron/ipc/systemHandlers.js
+++ app/electron/ipc/systemHandlers.js
@@ -1,5 +1,7 @@
 const dataService = require("../services/dataService");
 const uploadService = require("../services/uploadService");
+const { spawnSync } = require("child_process");
+const pythonService = require("../services/pythonService");
 
 function registerSystemHandlers(ipcMain)
 {
@@ -18,9 +20,31 @@ function registerSystemHandlers(ipcMain)
         return dataService.loadWorkflowSettings();
     });
 
-    ipcMain.handle("save-workflow-settings", async function handleSaveWorkflowSettings(settings)
+    ipcMain.handle("save-workflow-settings", async function handleSaveWorkflowSettings(_event, settings)
     {
         return dataService.saveWorkflowSettings(settings);
+    });
+
+    ipcMain.handle("run-env-check", async function handleRunEnvCheck(_event)
+    {
+        const pythonResult = await pythonService.resolvePythonCommand();
+        const ffmpegResult = spawnSync('ffmpeg', ['-version'], { windowsHide: true });
+        const ffprobeResult = spawnSync('ffprobe', ['-version'], { windowsHide: true });
+
+        return {
+            python: {
+                found: pythonResult.found,
+                version: pythonResult.version
+            },
+            ffmpeg: {
+                found: !ffmpegResult.error,
+                version: ffmpegResult.stdout.toString().split('\n')[0].trim()
+            },
+            ffprobe: {
+                found: !ffprobeResult.error,
+                version: ffprobeResult.stdout.toString().split('\n')[0].trim()
+            }
+        };
     });
 }
 
